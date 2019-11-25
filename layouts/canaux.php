ee1402fa10fb66528dda1c734855dbee344a99f1
Nassim <nassimelalam@gmail.com>
2019-11-25 14:08:28 +0100

    layouts base

---
+++ layouts/canaux.php
@@ -0,0 +1,61 @@
+<div class="inbox_people">
+    <div class="headind_srch">
+        <div class="recent_heading">
+            <h4>Liste des canaux</h4>
+        </div>
+        <div class="srch_bar">
+            <div class="stylish-input-group">
+                <input type="text" class="search-bar" placeholder="Rechercher">
+                <span class="input-group-addon">
+                    <button type="button"> <i class="fa fa-search" aria-hidden="true"></i> </button>
+                </span> </div>
+        </div>
+    </div>
+    <div class="inbox_chat">
+        <div class="chat_list active_chat">
+            <div class="chat_people">
+                <div class="chat_img"> <img
+                        src="https://cdn.pixabay.com/photo/2018/08/25/18/07/gamepad-3630565_960_720.png" alt="sunil">
+                </div>
+                <div class="chat_ib">
+                    <h5>Playstation <span class="chat_date">Dec 25</span></h5>
+                    <p>Room de chat entre les joueurs de Playstation.</p>
+                </div>
+            </div>
+        </div>
+        <div class="chat_list">
+            <div class="chat_people">
+                <div class="chat_img"> <img
+                        src="https://cdn.pixabay.com/photo/2016/06/29/14/17/joystick-1486908_1280.png" alt="sunil">
+                </div>
+                <div class="chat_ib">
+                    <h5>Xbox <span class="chat_date">Dec 25</span></h5>
+                    <p>Room de chat entre les joueurs de Xbox.</p>
+                </div>
+            </div>
+        </div>
+        <div class="chat_list">
+            <div class="chat_people">
+                <div class="chat_img"> <img
+                        src="https://cdn.pixabay.com/photo/2016/06/29/14/17/joystick-1486908_1280.png" alt="sunil">
+                </div>
+                <div class="chat_ib">
+                    <h5>Manga <span class="chat_date">Dec 25</span></h5>
+                    <p>Room de chat entre les amateurs de Manga</p>
+                </div>
+            </div>
+        </div>
+        <div class="chat_list">
+            <div class="chat_people">
+                <div class="chat_img"> <img
+                        src="https://cdn.pixabay.com/photo/2018/08/25/18/07/gamepad-3630565_960_720.png" alt="sunil">
+                </div>
+                <div class="chat_ib">
+                    <h5>Jeux PC<span class="chat_date">Dec 25</span></h5>
+                    <p>Room de chat entre les joueurs PC</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
